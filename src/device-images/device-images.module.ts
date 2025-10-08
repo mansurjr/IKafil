9f2deb7a07b6d30c57c86836c568dabcbbaee0fd
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-10-08 11:04:46 +0500

    installment_plans and device_images crud

--- src/device-images/device-images.module.ts
+++ src/device-images/device-images.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { DeviceImagesService } from './device-images.service';
-import { DeviceImagesController } from './device-images.controller';
+import { Module } from "@nestjs/common";
+import { DeviceImagesService } from "./device-images.service";
+import { DeviceImagesController } from "./device-images.controller";
+import { PrismaModule } from "../prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+  imports: [PrismaModule, JwtModule.register({})],
   controllers: [DeviceImagesController],
   providers: [DeviceImagesService],
+  exports: [DeviceImagesService],
 })
 export class DeviceImagesModule {}
--- src/installment-plans/installment-plans.module.ts
+++ src/installment-plans/installment-plans.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { InstallmentPlansService } from './installment-plans.service';
 import { InstallmentPlansController } from './installment-plans.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [PrismaModule, JwtModule.register({})],
   controllers: [InstallmentPlansController],
   providers: [InstallmentPlansService],
+  exports: [InstallmentPlansService]
 })
 export class InstallmentPlansModule {}
