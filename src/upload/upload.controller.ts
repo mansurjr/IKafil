176be3f3087993cf1a570f43a6ea20bb1023dd4f
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-10-10 00:14:58 +0500

    image upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -12,7 +12,7 @@ import { extname } from "path";
 import { UploadService } from "./upload.service";
 import { ApiConsumes, ApiBody, ApiTags } from "@nestjs/swagger";
 
-@ApiTags("Upload") 
+@ApiTags("Upload")
 @Controller("upload")
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
@@ -31,17 +31,19 @@ export class UploadController {
   @UseInterceptors(
     FileInterceptor("file", {
       storage: diskStorage({
-        destination: "./uploads/devices",
+        destination: "./uploads/devices", 
         filename: (req, file, cb) => {
           const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
-          const ext = extname(file.originalname);
-          cb(null, `${unique}${ext}`);
+          const ext = extname(file.originalname); 
+          cb(null, `${unique}${ext}`); 
         },
       }),
       fileFilter: (req, file, cb) => {
         if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
           return cb(
-            new BadRequestException("Rasm formati bo'lishi kerak"),
+            new BadRequestException(
+              "Rasm formati bo'lishi kerak (jpg, png, gif)"
+            ),
             false
           );
         }
