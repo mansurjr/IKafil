84b71f854a34c6bb90641765e11c14738af7102a
mansurjr <ztm007uzb@gmail.com>
2025-10-23 18:18:25 +0500

    removed notifiactions and guard in regions

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class NotificationsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   async create(createNotificationDto: CreateNotificationDto) {
     const notification = await this.prisma.notifications.create({
@@ -30,23 +30,6 @@ export class NotificationsService {
     return this.prisma.notifications.update({
       where: { id },
       data: { is_read: true },
-    });
-  }
-
-  async getNotificationsByStatus(
-    user_id: number,
-    is_read?: boolean,
-    pagination: { limit?: number; skip?: number } = {}
-  ) {
-    const { limit = 25, skip = 0 } = pagination;
-
-    const whereClause = typeof is_read === "boolean" ? { is_read } : {};
-
-    return await this.prisma.notifications.findMany({
-      where: { ...whereClause, user_id },
-      skip,
-      take: limit,
-      orderBy: { created_at: "desc" },
     });
   }
 }
